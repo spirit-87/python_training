ffa0c06993d86e1c67043ee49d2b2e51725dc3b7
elena <spirit_87@list.ru>
2020-03-04 10:54:15 +0300

    Выделен класс SessionHelper

--- fixture/application.py
+++ fixture/application.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.chrome.webdriver import WebDriver
-
+from fixture.session import SessionHelper
 
 class Application:
 
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
@@ -39,19 +40,6 @@ class Application:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
     def destroy(self):
         self.wd.quit()
