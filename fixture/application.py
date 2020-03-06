51e5253d046344171437bc83b4953c31b667a69d
elena <spirit_87@list.ru>
2020-03-06 09:01:25 +0300

    Добавлена проверка нахождения на главной странице в методе перехода на главную страницу

--- fixture/application.py
+++ fixture/application.py
@@ -15,7 +15,8 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost:8080/addressbook/index.php")
+        if not wd.current_url.endswith("/index.php") > 0:
+            wd.get("http://localhost:8080/addressbook/index.php")
 
     def destroy(self):
         self.wd.quit()
