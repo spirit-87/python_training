aeda2b389177421076b09cf8f831b067e7202357
elena <spirit_87@list.ru>
2020-04-03 15:38:32 +0300

    Добавлена возможность указывать браузер и базовый урл из командной строки или опций теста

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
@@ -7,16 +7,24 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver()
+    def __init__(self, browser, base_url):
+        if browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def open_home_page(self):
         wd = self.wd
         if not wd.current_url.endswith("/index.php") > 0:
-            wd.get("http://localhost:8080/addressbook/index.php")
+            wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
