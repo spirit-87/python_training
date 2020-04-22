bb5b6f7ec838e723ef7da4943e481c8a17022f22
elena <spirit_87@list.ru>
2020-04-22 12:25:45 +0300

    Добавлены шаги в тесте по добавлению группы для allure и добавлена папка со скриптами для allure  и отчетом allure

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import re
+import pytest
 
 
 
@@ -14,17 +15,20 @@ import re
 def test_add_group(app, db, check_ui, json_groups):
     #pass
     group = json_groups
-    old_groups = db.get_group_list()
-    app.group.create(group)
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    # для сортировки нужен ключ, если у группы нет id, то непонятно, как сортировать, но новая группа должна быть на последнем месте
-    # и иметь самый большой id
-    assert sorted(old_groups, key = Group.id_or_max) == sorted(new_groups, key = Group.id_or_max)
+    with pytest.allure.step('Given a group list'):
+        old_groups = db.get_group_list()
+    with pytest.allure.step('When I add the group %s to the list' % group):
+        app.group.create(group)
+    with pytest.allure.step('Then the new group list is equal to the old list with added group'):
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        # для сортировки нужен ключ, если у группы нет id, то непонятно, как сортировать, но новая группа должна быть на последнем месте
+        # и иметь самый большой id
+        assert sorted(old_groups, key = Group.id_or_max) == sorted(new_groups, key = Group.id_or_max)
 
-    # для отключаемой проверки, где сраниваем БД инфу с UI, надо преобразовать список, взятый из БД, - взять только id и name
-    if check_ui:
-        new_groups_ui = []
-        for i in new_groups:
-            new_groups_ui.append(Group(id=i.id, name=re.sub("  ", " ", i.name.strip())))
-        assert sorted(new_groups_ui, key = Group.id_or_max) == sorted(app.group.get_group_list(), key = Group.id_or_max)
+        # для отключаемой проверки, где сраниваем БД инфу с UI, надо преобразовать список, взятый из БД, - взять только id и name
+        if check_ui:
+            new_groups_ui = []
+            for i in new_groups:
+                new_groups_ui.append(Group(id=i.id, name=re.sub("  ", " ", i.name.strip())))
+            assert sorted(new_groups_ui, key = Group.id_or_max) == sorted(app.group.get_group_list(), key = Group.id_or_max)
